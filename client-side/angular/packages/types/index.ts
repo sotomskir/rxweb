73b8578d2088638703303dbfff69399008bad100
Robert Sotomski <sotomskir@users.noreply.github.com>
2021-02-19 12:21:02 +0100

    add i-abstract-control export

--- client-side/angular/packages/types/index.ts
+++ client-side/angular/packages/types/index.ts
@@ -1,8 +1,7 @@
-export * from './reactive-form/i-form-group';
+export * from './reactive-form/i-abstract-control';
 export * from './reactive-form/i-form-array';
 export * from './reactive-form/i-form-builder';
 export * from './reactive-form/control-prop';
 export * from './reactive-form/i-form-control';
 export * from './reactive-form/control-state';
 export * from './reactive-form/i-control-value-accessor'
-
